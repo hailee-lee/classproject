0ceaa52fc064c50a29dbad62427f5e274fe4b138
mouuaw <mouuaw@gmail.com>
2021-10-30 13:56:15 +0900

    update: javascript day1

---
+++ javascript/day1/project/ex2.js
@@ -0,0 +1,100 @@
+// ------ 산술 연산자
+
+// let a = 1 + 2;
+// console.log('a: ', a);
+
+// let b = 2 - 1;
+// console.log('b: ', b);
+
+// let c = 3 * 4;
+// console.log('c: ', c);
+
+// let d = 10 / 4;
+// console.log('d: ', d);
+
+// let e = 10 % 4;
+// console.log('e: ', e);
+
+
+// ------ 문자열 연산자
+
+// let strA = 'hello ' + 'world'
+// console.log('strA: ', strA);
+
+// let strB = 'this number is ' + 10
+// console.log('strB: ', strB);
+
+// let strC = 20 + ' is my age'
+// console.log('strC: ', strC);
+
+
+// ------ 증감 연산자
+
+// let number = 1
+// number++
+// console.log('number: ', number);
+// console.log('number: ', number++);
+// console.log('number: ', number);
+// console.log('number: ', ++number);
+// console.log('number: ', number);
+
+
+// ------ 대입 연산자
+// let number = 1
+// number += 1;
+// console.log('number += ', number);
+
+// number -= 1;
+// console.log('number -= ', number);
+
+// number *= 10;
+// console.log('number *= ', number);
+
+// number /= 10;
+// console.log('number /= ', number);
+
+
+// ------ 비교 연산자
+// let betweenA = 1
+// let betweenB = 2
+
+// // betweenA == betweenB
+// console.log('betweenA == betweenB: ', betweenA == betweenB);
+
+// // betweenA != betweenB
+// console.log('betweenA != betweenB: ', betweenA != betweenB);
+
+// // betweenA > betweenB
+// console.log('betweenA > betweenB: ', betweenA > betweenB);
+
+// // betweenA < betweenB
+// console.log('betweenA < betweenB: ', betweenA < betweenB);
+
+// // betweenA >= betweenB
+// console.log('betweenA >= betweenB: ', betweenA >= betweenB);
+
+// // betweenA <= betweenB
+// console.log('betweenA <= betweenB: ', betweenA <= betweenB);
+
+
+// ------ 삼항 연산자
+// let number = 10
+// let truefalse = number > 10 ? 20 : 1
+// console.log('truefalse: ', truefalse);
+
+
+// ------ 논리 연산자
+// let numberA = 10
+// let numberB = 20
+
+// // (numberA == numberB) && (numberA != numberB)
+// console.log('(numberA == numberB) && (numberA != numberB): ', (numberA == numberB) && (numberA != numberB));
+
+// // (numberA == numberB) || (numberA != numberB)
+// console.log('(numberA == numberB) || (numberA != numberB): ', (numberA == numberB) || (numberA != numberB));
+
+// // !numberA
+// console.log('!numberA: ', !numberA);
+
+// // !!numberA
+// console.log('!!numberA: ', !!numberA);
---
+++ javascript/day1/project/ex4.js
@@ -0,0 +1,28 @@
+const divList = document.getElementsByTagName('div')
+console.log('divList: ', divList);
+console.log('divList[0]: ', divList[0]);
+
+const spanList = document.getElementsByTagName('span')
+console.log('spanList: ', spanList);
+
+const divFromClass = document.getElementsByClassName('div-class')
+console.log('divFromClass: ', divFromClass);
+
+const pFromClass = document.getElementsByClassName('p-class')
+console.log('pFromClass: ', pFromClass);
+
+const h1FromId = document.getElementById('h1-id')
+console.log('h1FromId: ', h1FromId);
+
+const h3FromId = document.getElementById('h3-id')
+console.log('h3FromId: ', h3FromId);
+
+const someList = document.querySelector('.some-list')
+console.log('someList: ', someList);
+
+const liList = document.querySelectorAll('li')
+console.log('liList: ', liList);
+console.log('liList[0]: ', liList[0]);
+
+const liThird = document.querySelector('.li-third')
+console.log('liThird: ', liThird);
