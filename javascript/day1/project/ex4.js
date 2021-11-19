14a260bbfb964be9dbbe02250bb21b37c09719f7
yulim <lucy98002808@gmail.com>
2021-11-19 16:12:53 +0900

    mypractice

--- javascript/day1/project/ex4.js
+++ javascript/day1/project/ex4.js
@@ -1,28 +1,35 @@
-const divList = document.getElementsByTagName('div')
-console.log('divList: ', divList);
-console.log('divList[0]: ', divList[0]);
+// const divList = document.getElementsByTagName('div')
+// console.log('divList: ', divList);
+// console.log('divList[0]: ', divList[0]);
 
-const spanList = document.getElementsByTagName('span')
-console.log('spanList: ', spanList);
+// const spanList = document.getElementsByTagName('span')
+// console.log('spanList: ', spanList);
 
-const divFromClass = document.getElementsByClassName('div-class')
-console.log('divFromClass: ', divFromClass);
+// const divFromClass = document.getElementsByClassName('div-class')
+// console.log('divFromClass: ', divFromClass);
 
-const pFromClass = document.getElementsByClassName('p-class')
-console.log('pFromClass: ', pFromClass);
+// const pFromClass = document.getElementsByClassName('p-class')
+// console.log('pFromClass: ', pFromClass);
 
-const h1FromId = document.getElementById('h1-id')
-console.log('h1FromId: ', h1FromId);
+// const h1FromId = document.getElementById('h1-id')
+// console.log('h1FromId: ', h1FromId);
 
-const h3FromId = document.getElementById('h3-id')
-console.log('h3FromId: ', h3FromId);
+// const h3FromId = document.getElementById('h3-id')
+// console.log('h3FromId: ', h3FromId);
 
-const someList = document.querySelector('.some-list')
-console.log('someList: ', someList);
+// const someList = document.querySelector('.some-list')
+// console.log('someList: ', someList);
 
-const liList = document.querySelectorAll('li')
-console.log('liList: ', liList);
-console.log('liList[0]: ', liList[0]);
+// const liList = document.querySelectorAll('li')
+// console.log('liList: ', liList);
+// console.log('liList[0]: ', liList[0]);
 
-const liThird = document.querySelector('.li-third')
-console.log('liThird: ', liThird);
+// const liThird = document.querySelector('.li-third')
+// console.log('liThird: ', liThird);
+
+const yulim = document.querySelector('.div-class')
+console.log('yulim:', yulim);
+const yulim1 = document.querySelector('.some-list')
+console.log('yulim1', yulim1);
+const yulim2 = document.querySelectorAll('.some-item.li-third')
+console.log('yulim2:', yulim2);
