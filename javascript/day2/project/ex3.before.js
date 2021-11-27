17f9ec71b1637e894e058c97e5fbf4fae2762054
yulim <lucy98002808@gmail.com>
2021-11-27 14:49:56 +0900

    yulim

---
+++ javascript/day2/project/ex3.before.js
@@ -0,0 +1,27 @@
+let Number = 3
+
+function number3check(number) {
+    if (number == 3) {
+        console.log('this is three')
+    } else {
+        console.log('this is not thrree')
+    }
+}
+
+
+number3check(3)
+
+function numbercheck(number) {
+    if (number == 1) {
+        console.log('this is one')
+    } else if (number == 2) {
+        console.log('this is two')
+    } else if (number == 3) {
+        console.log('this is three')
+    } else {
+        console.log('what ???')
+    }
+}
+
+const myNumber = 2
+numbercheck(myNumber)
